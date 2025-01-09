24b7a646b7478f5437df68e557ec47cff7b02bc4
Prince Yadav <asdfghjkl890890@proton.me>
2025-01-09 11:54:03 +0530

    Arrange folder

--- employee/src/main/java/org/prince/employee/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package org.prince.employee.repo;
-
-import org.prince.employee.emp.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpRepo extends JpaRepository<Employee, Integer> {
-}
